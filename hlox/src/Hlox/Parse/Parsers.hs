9c06d74728553689763115d5c53a2fb89284a8d6
Andrei Dorian Duma <andrei@andreiduma.ro>
2023-03-16 11:34:55 +0200

    Hlox: attempt a tagless final approach; stuck

--- hlox/src/Hlox/Parse/Parsers.hs
+++ hlox/src/Hlox/Parse/Parsers.hs
@@ -4,127 +4,158 @@ module Hlox.Parse.Parsers where
 
 import Hlox.Parse.Lexer (identifierP, lexeme, numberP, spaceP, symbol)
 import Hlox.Parse.Types (Parser)
-import Hlox.Syntax (Decl (..), Expr (..), Program (..), Stmt (..))
+import Hlox.Syntax
 
 import Data.Foldable (foldl')
 import Data.Function ((&))
+import Hlox.Syntax (LiteralNil)
 import Text.Megaparsec (choice, eof, many, optional, single, takeWhileP, try, (<|>))
 
--- Program
+-- -- Program
 
-programP :: Parser Program
-programP = spaceP *> (Program <$> many declarationP) <* eof
+-- programP :: Parser p
+-- programP = spaceP *> (Program <$> many declarationP) <* eof
 
--- Declarations
+-- -- Declarations
 
-declarationP :: Parser Decl
-declarationP =
-    choice
-        [ varDeclP
-        , statementP
-        ]
+-- declarationP :: Parser d
+-- declarationP =
+--     choice
+--         [ varDeclP
+--         , statementP
+--         ]
 
-varDeclP :: Parser Decl
-varDeclP =
-    VarDecl
-        <$> (symbol "var" *> identifierP)
-        <*> optional (symbol "=" *> exprP)
-        <* symbol ";"
+-- varDeclP :: Parser d
+-- varDeclP =
+--     VarDecl
+--         <$> (symbol "var" *> identifierP)
+--         <*> optional (symbol "=" *> exprP)
+--         <* symbol ";"
 
-statementP :: Parser Decl
-statementP = StmtDecl <$> stmtP
+-- statementP :: Parser d
+-- statementP = StmtDecl <$> stmtP
 
--- Statements
+-- -- Statements
 
-stmtP :: Parser Stmt
-stmtP =
-    choice
-        [ blockStmtP
-        , ifStmtP
-        , whileStmt
-        , printStmtP
-        , exprStmtP
-        ]
+-- stmtP :: Parser s
+-- stmtP =
+--     choice
+--         [ blockStmtP
+--         , ifStmtP
+--         , whileStmt
+--         , printStmtP
+--         , exprStmtP
+--         ]
 
-blockStmtP :: Parser Stmt
-blockStmtP = BlockStmt <$> (symbol "{" *> many declarationP <* symbol "}")
+-- blockStmtP :: Parser s
+-- blockStmtP = BlockStmt <$> (symbol "{" *> many declarationP <* symbol "}")
 
-ifStmtP :: Parser Stmt
-ifStmtP =
-    IfStmt
-        <$> (symbol "if" *> symbol "(" *> exprP <* symbol ")")
-        <*> stmtP
-        <*> optional (symbol "else" *> stmtP)
+-- ifStmtP :: Parser s
+-- ifStmtP =
+--     IfStmt
+--         <$> (symbol "if" *> symbol "(" *> exprP <* symbol ")")
+--         <*> stmtP
+--         <*> optional (symbol "else" *> stmtP)
 
-whileStmt :: Parser Stmt
-whileStmt = WhileStmt <$> (symbol "while" *> symbol "(" *> exprP <* symbol ")") <*> stmtP
+-- whileStmt :: Parser s
+-- whileStmt = WhileStmt <$> (symbol "while" *> symbol "(" *> exprP <* symbol ")") <*> stmtP
 
-printStmtP :: Parser Stmt
-printStmtP = PrintStmt <$> (symbol "print" *> exprP <* symbol ";")
+-- printStmtP :: Parser s
+-- printStmtP = PrintStmt <$> (symbol "print" *> exprP <* symbol ";")
 
-exprStmtP :: Parser Stmt
-exprStmtP = ExprStmt <$> (exprP <* symbol ";")
+-- exprStmtP :: Parser s
+-- exprStmtP = ExprStmt <$> (exprP <* symbol ";")
 
--- Expressions
+-- -- Expressions
 
-exprP :: Parser Expr
-exprP = assignmentP
+-- exprP :: Parser e
+-- exprP = assignmentP
 
-assignmentP :: Parser Expr
-assignmentP =
-    choice
-        [ try $ Assignment <$> identifierP <*> (symbol "=" *> exprP)
-        , logicOrP
-        ]
+-- assignmentP :: Parser e
+-- assignmentP =
+--     choice
+--         [ try $ Assignment <$> identifierP <*> (symbol "=" *> exprP)
+--         , logicOrP
+--         ]
 
-logicOrP :: Parser Expr
-logicOrP = binaryExpr logicAndP (Or <$ symbol "or")
+-- logicOrP :: Parser e
+-- logicOrP = binaryExpr logicAndP (Or <$ symbol "or")
 
-logicAndP :: Parser Expr
-logicAndP = binaryExpr equalityP (And <$ symbol "and")
+-- logicAndP :: Parser e
+-- logicAndP = binaryExpr equalityP (And <$ symbol "and")
 
-equalityP :: Parser Expr
-equalityP = binaryExpr comparisonP (Equal <$ symbol "==" <|> NotEqual <$ symbol "!=")
+-- equalityP :: Parser e
+-- equalityP = binaryExpr comparisonP (Equal <$ symbol "==" <|> NotEqual <$ symbol "!=")
 
-comparisonP :: Parser Expr
-comparisonP =
-    binaryExpr termP $
-        choice
-            [ GreaterOrEqual <$ symbol ">="
-            , Greater <$ symbol ">"
-            , Less <$ symbol "<"
-            , LessOrEqual <$ symbol "<="
-            ]
+-- comparisonP :: Parser e
+-- comparisonP =
+--     binaryExpr termP $
+--         choice
+--             [ GreaterOrEqual <$ symbol ">="
+--             , Greater <$ symbol ">"
+--             , Less <$ symbol "<"
+--             , LessOrEqual <$ symbol "<="
+--             ]
 
-termP :: Parser Expr
-termP = binaryExpr factorP (Plus <$ symbol "+" <|> Minus <$ symbol "-")
+-- termP :: Parser e
+-- termP = binaryExpr factorP (Plus <$ symbol "+" <|> Minus <$ symbol "-")
 
-factorP :: Parser Expr
-factorP = binaryExpr unaryP (Star <$ symbol "*" <|> Slash <$ symbol "/")
+-- factorP :: Parser e
+-- factorP = binaryExpr unaryP (Star <$ symbol "*" <|> Slash <$ symbol "/")
 
-unaryP :: Parser Expr
-unaryP =
-    choice
-        [ Not <$> (symbol "!" *> unaryP)
-        , Negate <$> (symbol "-" *> unaryP)
-        , primaryP
-        ]
+-- unaryP :: Parser e
+-- unaryP =
+--     choice
+--         [ Not <$> (symbol "!" *> unaryP)
+--         , Negate <$> (symbol "-" *> unaryP)
+--         , primaryP
+--         ]
 
-primaryP :: Parser Expr
+-- primaryP :: Parser e
+-- primaryP =
+--     choice
+--         [ LiteralNil <$ symbol "nil"
+--         , LiteralBoolean <$> (True <$ symbol "true" <|> False <$ symbol "false")
+--         , LiteralNumber <$> numberP
+--         , LiteralString <$> lexeme (single '"' *> takeWhileP Nothing (/= '"') <* single '"')
+--         , Identifier <$> identifierP
+--         , Grouping <$> (symbol "(" *> exprP <* symbol ")")
+--         ]
+
+-- -- Utilities
+
+-- binaryExpr :: Parser e -> Parser (e -> e -> e) -> Parser e
+-- binaryExpr operandP binaryConsP = do
+--     left <- operandP
+--     rest <- many (flip <$> binaryConsP <*> operandP)
+--     pure $ foldl' (&) left rest
+
+------------------------------------------------------------------------------
+
+-- primaryP :: Parser e
+-- primaryP =
+--     choice
+--         [ LiteralNil <$ symbol "nil"
+--         , LiteralBoolean <$> (True <$ symbol "true" <|> False <$ symbol "false")
+--         , LiteralNumber <$> numberP
+--         , LiteralString <$> lexeme (single '"' *> takeWhileP Nothing (/= '"') <* single '"')
+--         , Identifier <$> identifierP
+--         -- , Grouping <$> (symbol "(" *> exprP <* symbol ")")
+--         ]
+
+class Expr e => Parse e where
+    parse :: Parser e
+
+instance Parse LiteralNil where
+    parse :: Parser LiteralNil
+    parse = LiteralNil <$ symbol "nil"
+
+instance Parse LiteralBoolean where
+    parse :: Parser LiteralBoolean
+    parse = LiteralBoolean <$> (True <$ symbol "true" <|> False <$ symbol "false")
+
 primaryP =
     choice
         [ LiteralNil <$ symbol "nil"
         , LiteralBoolean <$> (True <$ symbol "true" <|> False <$ symbol "false")
-        , LiteralNumber <$> numberP
-        , LiteralString <$> lexeme (single '"' *> takeWhileP Nothing (/= '"') <* single '"')
-        , Identifier <$> identifierP
-        , Grouping <$> (symbol "(" *> exprP <* symbol ")")
         ]
-
--- Utilities
-
-binaryExpr :: Parser Expr -> Parser (Expr -> Expr -> Expr) -> Parser Expr
-binaryExpr operandP binaryConsP = do
-    left <- operandP
-    rest <- many (flip <$> binaryConsP <*> operandP)
-    pure $ foldl' (&) left rest
--- hlox/src/Hlox/Syntax.hs
+++ hlox/src/Hlox/Syntax.hs
@@ -1,54 +1,119 @@
-module Hlox.Syntax (
-    Program (..),
-    Decl (..),
-    Stmt (..),
-    Expr (..),
-) where
+-- TODO: Explicit exports
+module Hlox.Syntax where
 
 import Data.Text (Text)
 
-newtype Program = Program [Decl]
-    deriving (Eq, Show)
+----------- Program -----------
 
-data Decl
-    = VarDecl Text (Maybe Expr)
-    | StmtDecl Stmt
-    deriving (Eq, Show)
+data Program d = Program [d]
 
-data Stmt
-    = BlockStmt [Decl]
-    | IfStmt Expr Stmt (Maybe Stmt)
-    | WhileStmt Expr Stmt
-    | PrintStmt Expr
-    | ExprStmt Expr
-    deriving (Eq, Show)
+---- Program type class and instances ----
 
-data Expr
-    = Identifier Text
-    | -- Literals
-      LiteralNil
-    | LiteralBoolean Bool
-    | LiteralNumber Double
-    | LiteralString Text
-    | -- Boolean operators
-      Not Expr
-    | And Expr Expr
-    | Or Expr Expr
-    | -- Number operators
-      Negate Expr
-    | Plus Expr Expr
-    | Minus Expr Expr
-    | Star Expr Expr
-    | Slash Expr Expr
-    | -- Comparison operators
-      Equal Expr Expr
-    | NotEqual Expr Expr
-    | Less Expr Expr
-    | LessOrEqual Expr Expr
-    | Greater Expr Expr
-    | GreaterOrEqual Expr Expr
-    | -- Assignment
-      Assignment Text Expr
-    | -- Parentheses
-      Grouping Expr
-    deriving (Eq, Show)
+class Prog p
+
+instance Decl d => Prog (Program d)
+
+----------- Declarations -----------
+
+data VarDecl e = VarDecl Text (Maybe e)
+data StmtDecl s = StmtDecl s
+
+---- Decl type class and instances ----
+
+class Decl d
+
+instance Expr e => Decl (VarDecl e)
+instance Stmt s => Decl (StmtDecl s)
+
+----------- Statements -----------
+
+data BlockStmt d = BlockStmt [d]
+data IfStmt e s = IfStmt e s (Maybe s)
+data WhileStmt e s = WhileStmt e s
+data PrintStmt e = PrintStmt e
+data ExprStmt e = ExprStmt e
+
+---- Stmt type class and instances ----
+
+class Stmt s
+
+instance Decl d => Stmt (BlockStmt d)
+instance (Expr e, Stmt s) => Stmt (IfStmt e s)
+instance (Expr e, Stmt s) => Stmt (WhileStmt e s)
+instance Expr e => Stmt (PrintStmt e)
+instance Expr e => Stmt (ExprStmt e)
+
+----------- Expressions -----------
+
+-- Identifiers
+data Identifier = Identifier Text
+
+-- Literals
+data LiteralNil = LiteralNil
+data LiteralBoolean = LiteralBoolean Bool
+data LiteralNumber = LiteralNumber Double
+data LiteralString = LiteralString Text
+
+-- Boolean operators
+data Not e = Not e
+data And l r = And l r
+data Or l r = Or l r
+
+-- Number operators
+data Negate e = Negate e
+data Plus l r = Plus l r
+data Minus l r = Minus l r
+data Star l r = Star l r
+data Slash l r = Slash l r
+
+-- Comparison operators
+data Equal l r = Equal l r
+data NotEqual l r = NotEqual l r
+data Less l r = Less l r
+data LessOrEqual l r = LessOrEqual l r
+data Greater l r = Greater l r
+data GreaterOrEqual l r = GreaterOrEqual l r
+
+-- Assignment
+data Assignment e = Assignment Text e
+
+-- Parentheses
+data Grouping e = Grouping e
+
+---- Expr type class and instances ----
+class Expr e
+
+-- Identifiers
+instance Expr Identifier
+
+-- Literals
+instance Expr LiteralNil
+instance Expr LiteralBoolean
+instance Expr LiteralNumber
+instance Expr LiteralString
+
+-- Boolean operators
+instance Expr e => Expr (Not e)
+instance (Expr l, Expr r) => Expr (And l r)
+instance (Expr l, Expr r) => Expr (Or l r)
+
+-- Number operators
+instance Expr e => Expr (Negate e)
+instance (Expr l, Expr r) => Expr (Plus l r)
+instance (Expr l, Expr r) => Expr (Minus l r)
+instance (Expr l, Expr r) => Expr (Star l r)
+instance (Expr l, Expr r) => Expr (Slash l r)
+
+-- Comparison operators
+instance (Expr l, Expr r) => Expr (Equal l r)
+instance (Expr l, Expr r) => Expr (NotEqual l r)
+instance (Expr l, Expr r) => Expr (Less l r)
+instance (Expr l, Expr r) => Expr (LessOrEqual l r)
+instance (Expr l, Expr r) => Expr (Greater l r)
+instance (Expr l, Expr r) => Expr (GreaterOrEqual l r)
+
+-- Assignment
+instance Expr e => Expr (Assignment e)
+
+-- Parentheses
+instance Expr e => Expr (Grouping e)
